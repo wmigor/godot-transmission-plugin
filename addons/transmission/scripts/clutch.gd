bee42d90770e40f91256aefe3466281dd500f516
igor <wmigor@yandex.ru>
2026-01-27 00:32:35 +0300

    fix clutch lock

--- addons/transmission/scripts/clutch.gd
+++ addons/transmission/scripts/clutch.gd
@@ -17,19 +17,19 @@ func calculate(delta: float, motor: Motor, differential: Differential, gear: flo
 	var axle_av := differential.get_axle_angular_velocity() * gear
 	var axle_torque := differential.get_axle_torque() / gear
 	var av_delta := axle_av - motor.angular_velocity
+	var axle_inertia := differential.get_axle_inertia() / gear
 	if not clutch_locked or clutch_locked and absf(motor.torque - axle_torque / gear) > clutch_max_torque:
 		clutch_locked = false
 		var clutch_torque := clampf(signf(av_delta) * clutch_max_torque, -clutch_max_torque, clutch_max_torque)
 		var motor_torque := motor.torque + clutch_torque
 		var differential_torque := -clutch_torque * gear
 		var new_motor_av := motor.angular_velocity + delta * motor_torque / motor.inertia
-		var new_differential_av := axle_av + delta * differential_torque / differential_torque
+		var new_differential_av := axle_av + delta * differential_torque / axle_inertia
 		var new_av_delta := new_differential_av - new_motor_av
 		if new_av_delta * av_delta >= 0.0:
 			motor.apply_torque(delta, motor_torque)
 			differential.apply_torque(differential_torque)
 			return
-	var axle_inertia := differential.get_axle_inertia() / gear
 	motor.angular_velocity = (motor.angular_velocity * motor.inertia + axle_av * axle_inertia) / (motor.inertia + axle_inertia)
 	clutch_locked = true
 	motor.apply_torque(delta, motor.torque * gear + axle_torque, motor.inertia + axle_inertia)
