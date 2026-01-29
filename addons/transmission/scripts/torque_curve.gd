908345a9504ad87bf9cfa013a114e7246c576a5a
igor <wmigor@yandex.ru>
2026-01-29 18:31:26 +0300

    torque curve

--- addons/transmission/scripts/torque_curve.gd
+++ addons/transmission/scripts/torque_curve.gd
@@ -2,7 +2,7 @@
 extends Resource
 class_name TorqueCurve
 
-@export var idle_torque := 80.0:
+@export var idle_torque := 70.0:
 	set(value):
 		idle_torque = value
 		emit_changed()
@@ -35,7 +35,25 @@ class_name TorqueCurve
 		emit_changed()
 
 
-@export_range(0.0, 1.0, 0.01) var drop_torque := 0.95:
+@export_range(0.0, 1.0, 0.01) var start_gain_torque_rpm := 0.23:
+	set(value):
+		start_gain_torque_rpm = value
+		emit_changed()
+
+
+@export_range(0.0, 1.0, 0.01) var gain_torque := 0.8:
+	set(value):
+		gain_torque = value
+		emit_changed()
+
+
+@export var gain_torque_power := 1.5:
+	set(value):
+		gain_torque_power = value
+		emit_changed()
+
+
+@export_range(0.0, 1.0, 0.01) var drop_torque := 0.9:
 	set(value):
 		drop_torque = value
 		emit_changed()
@@ -67,9 +85,14 @@ func get_torque(angular_velocity: float) -> float:
 	if rpm <= idle_rpm:
 		return idle_torque
 	var mid_rpm := max_rpm * peak_torque_rpm
+	var start_gain_rpm := max_rpm * start_gain_torque_rpm
+	var start_torque := gain_torque * max_torque
+	if rpm < start_gain_rpm:
+		var w := 1.0 - (rpm - idle_rpm) / (start_gain_rpm - idle_rpm)
+		return lerpf(idle_torque, start_torque, 1.0 - pow(w, gain_torque_power))
 	if rpm <= mid_rpm:
-		var w := 1.0 - (rpm - idle_rpm) / (mid_rpm - idle_rpm)
-		return lerpf(idle_torque, max_torque, 1.0 - pow(w, peak_torque_power))
+		var w := 1.0 - (rpm - start_gain_rpm) / (mid_rpm - start_gain_rpm)
+		return lerpf(start_torque, max_torque, 1.0 - pow(w, peak_torque_power))
 	else:
 		var w := (rpm - mid_rpm) / (max_rpm - mid_rpm)
 		return lerpf(max_torque, max_torque * drop_torque, pow(w, drop_torque_power))
