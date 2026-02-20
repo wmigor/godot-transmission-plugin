35d9e5a7fd3ac5eec68a30d5b8942c77dfce1f10
igor <wmigor@yandex.ru>
2026-02-20 13:12:57 +0300

    4x4

--- addons/transmission/scripts/differential.gd
+++ addons/transmission/scripts/differential.gd
@@ -4,103 +4,103 @@ class_name Differential
 
 enum Type {Auto, Open, Spool}
 
+@export var left_node: NodePath
+@export var right_node: NodePath
 @export var type := Type.Auto
 @export var auto_differential_lock_input_torque := 25.0
 @export var auto_differential_slip_max := 0.35
 
-var _wheels: WheelsSim
+var _left: Node
+var _right: Node
 var _vehicle: RigidBody3D
+var shaft := Shaft.new()
+var left_shaft := Shaft.new()
+var right_shaft := Shaft.new()
 
 
 func _ready() -> void:
-	_wheels = get_parent()
-	if _wheels == null:
+	_left = get_node(left_node)
+	_right = get_node(right_node)
+	if _left is Wheel or _left is Differential:
+		_left.shaft = left_shaft
+	if _right is Wheel or _right is Differential:
+		_right.shaft = right_shaft
+	var wheels := get_parent() as Wheels
+	if wheels == null:
 		return
-	var transmission := _wheels.transmission
+	var transmission := wheels.transmission
 	if transmission == null:
 		return
 	_vehicle = transmission.get_parent() as RigidBody3D
 
 
-func apply_torque(delta: float, input_torque: float) -> void:
-	if len(_wheels.traction_wheels) != 2:
-		return
+func apply_torque(delta: float) -> void:
+	var input_torque := shaft.torque
 	if type == Type.Spool:
 		var axle_av := get_axle_angular_velocity()
 		var axle_inertia := get_axle_inertia()
-		var axle_torque := get_axle_torque() * len(_wheels.traction_wheels)
-		axle_av += delta * (input_torque + axle_torque) / axle_inertia
-		_wheels.traction_wheels[0].torque = (axle_av - _wheels.traction_wheels[0].angular_velocity) * _wheels.traction_wheels[0].inertia / delta
-		_wheels.traction_wheels[1].torque = (axle_av - _wheels.traction_wheels[1].angular_velocity) * _wheels.traction_wheels[1].inertia / delta
+		axle_av += delta * input_torque / axle_inertia
+		left_shaft.torque = (axle_av - left_shaft.angular_velocity) * left_shaft.inertia / delta
+		right_shaft.torque = (axle_av - right_shaft.angular_velocity) * right_shaft.inertia / delta
 	elif type == Type.Open:
-		var spider_torque := _wheels.traction_wheels[0].torque - _wheels.traction_wheels[1].torque
+		var spider_torque := left_shaft.torque - right_shaft.torque
 		var torque0 := input_torque * 0.5 + spider_torque
 		var torque1 := input_torque * 0.5 - spider_torque
-		_wheels.traction_wheels[0].torque += torque0
-		_wheels.traction_wheels[1].torque += torque1
+		left_shaft.torque = torque0
+		right_shaft.torque = torque1
 	elif type == Type.Auto:
-		var spider_torque := _wheels.traction_wheels[0].torque - _wheels.traction_wheels[1].torque
+		var spider_torque := left_shaft.torque - right_shaft.torque
 		var locked_torque := input_torque / auto_differential_lock_input_torque
 		var rate := 1.0 - exp(-locked_torque * locked_torque) if locked_torque > 0.0 else 0.0
-		var pressure := tanh(rate * (_wheels.traction_wheels[1].angular_velocity - _wheels.traction_wheels[0].angular_velocity))
+		var pressure := tanh(rate * (right_shaft.angular_velocity - left_shaft.angular_velocity))
 		var bias := auto_differential_slip_max * 0.5 * pressure
 		var open := 1.0 - rate
 		var torque0 := input_torque * (0.5 + bias) + spider_torque * open
 		var torque1 := input_torque * (0.5 - bias) - spider_torque * open
-		_wheels.traction_wheels[0].torque += torque0
-		_wheels.traction_wheels[1].torque += torque1
-	for wheel in _wheels.traction_wheels:
-		wheel.apply_torque(delta)
+		left_shaft.torque = torque0
+		right_shaft.torque = torque1
+	_left.apply_torque(delta)
+	_right.apply_torque(delta)
+
+
+func update_shafts() -> void:
+	_left.update_shafts()
+	_right.update_shafts()
+	shaft.inertia = get_axle_inertia()
+	shaft.torque = get_axle_torque()
+	shaft.angular_velocity = get_axle_angular_velocity()
+	shaft.brake_torque = 0.5 * (left_shaft.brake_torque + right_shaft.brake_torque)
+
+
+func find_wheels(wheels: Array[Wheel]) -> void:
+	if _left is Wheel:
+		wheels.append(_left)
+	elif _left is Differential:
+		_left.find_wheels(wheels)
+	if _right is Wheel:
+		wheels.append(_right)
+	elif _right is Differential:
+		_right.find_wheels(wheels)
 
 
 func get_axle_inertia() -> float:
-	var axle_inertia := 0.0
-	for wheel in _wheels.traction_wheels:
-		axle_inertia += wheel.inertia
-	return axle_inertia
+	return left_shaft.inertia + right_shaft.inertia
 
 
 func get_axle_angular_velocity() -> float:
-	if len(_wheels.traction_wheels) < 1:
-		return 0.0
-	var axle_av := 0.0
-	for wheel in _wheels.traction_wheels:
-		axle_av += wheel.angular_velocity
-	return axle_av / len(_wheels.traction_wheels)
+	return 0.5 * (left_shaft.angular_velocity + right_shaft.angular_velocity)
 
 
 func get_axle_torque() -> float:
-	if len(_wheels.traction_wheels) < 1:
-		return 0.0
-	var axle_torque := 0.0
-	for wheel in _wheels.traction_wheels:
-		axle_torque += wheel.torque + wheel.brake_torque
-	return axle_torque / len(_wheels.traction_wheels)
-
-
-func before_simulation(delta: float, input_steering: float) -> void:
-	if _vehicle == null:
-		return
-	var state := PhysicsServer3D.body_get_direct_state(_vehicle.get_rid())
-	if state == null:
-		return
-	var center_of_mass := _vehicle.global_transform * state.center_of_mass_local
-	for wheel in _wheels.wheels:
-		if wheel.steer_angle_max != 0.0:
-			wheel.rotation_degrees.y = input_steering * wheel.steer_angle_max
-		wheel.calculate_force(delta, _vehicle, center_of_mass)
-
-
-func after_simulation(delta: float, free: bool, input_brake: float, input_hand_brake: float) -> void:
-	for wheel in _wheels.free_wheels:
-		wheel.apply_torque(delta)
-	for wheel in _wheels.wheels:
-		var brake := maxf(input_brake, input_hand_brake) if wheel.hand_brakable else input_brake
-		wheel.update_rotation(delta, free, brake)
+	return 0.5 * (left_shaft.torque + left_shaft.brake_torque + right_shaft.torque + right_shaft.brake_torque)
 
 
 func switch() -> void:
 	type = ((type as int + 1) % 3) as Type
+	if _left is Differential:
+		_left.type = type
+	if _right is Differential:
+		_right.type = type
 
 
 func get_type_name() -> String:
--- addons/transmission/scripts/wheels_sim.gd
+++ addons/transmission/scripts/wheels_sim.gd
@@ -7,44 +7,68 @@ class_name WheelsSim
 var traction_wheels: Array[Wheel]
 var free_wheels: Array[Wheel]
 var wheels: Array[Wheel]
+var _vehicle: RigidBody3D
 
 
 func _ready() -> void:
 	if transmission == null:
 		return
-	var vehicle := transmission.get_parent() as RigidBody3D
-	if vehicle == null:
+	_vehicle = transmission.get_parent()
+	if _vehicle == null:
 		return
-	for wheel in vehicle.find_children("*", "Wheel", false):
-		wheels.append(wheel)
-		if wheel.use_as_traction:
-			traction_wheels.append(wheel)
-		else:
+	_find_wheels()
+
+
+func _find_wheels() -> void:
+	traction_wheels.clear()
+	wheels.clear()
+	free_wheels.clear()
+	if differential != null:
+		differential.find_wheels(traction_wheels)
+		wheels.append_array(traction_wheels)
+	for wheel in _vehicle.find_children("*", "Wheel", false):
+		if wheel not in wheels:
+			wheels.append(wheel)
 			free_wheels.append(wheel)
 
 
-func apply_torque(delta: float, input_torque: float) -> void:
+func apply_torque(delta: float, torque: float) -> void:
 	if differential != null:
-		differential.apply_torque(delta, input_torque)
+		differential.shaft.torque = torque
+		differential.apply_torque(delta)
 
 
 func get_axle_inertia() -> float:
-	return differential.get_axle_inertia() if differential != null else 0.0
+	return differential.shaft.inertia if differential != null else 0.0
 
 
 func get_axle_angular_velocity() -> float:
-	return differential.get_axle_angular_velocity() if differential != null else 0.0
+	return differential.shaft.angular_velocity if differential != null else 0.0
 
 
 func get_axle_torque() -> float:
-	return differential.get_axle_torque() if differential != null else 0.0
+	return differential.shaft.torque if differential != null else 0.0
 
 
 func before_simulation(delta: float, input_steering: float) -> void:
+	if _vehicle == null:
+		return
+	var state := PhysicsServer3D.body_get_direct_state(_vehicle.get_rid())
+	if state == null:
+		return
+	var center_of_mass := _vehicle.global_transform * state.center_of_mass_local
+	for wheel in wheels:
+		if wheel.steer_angle_max != 0.0:
+			wheel.rotation_degrees.y = input_steering * wheel.steer_angle_max
+		wheel.calculate_force(delta, _vehicle, center_of_mass)
 	if differential != null:
-		differential.before_simulation(delta, input_steering)
+		differential.update_shafts()
 
 
 func after_simulation(delta: float, free: bool, input_brake: float, input_hand_brake: float) -> void:
-	if differential != null:
-		differential.after_simulation(delta, free, input_brake, input_hand_brake)
+	for wheel in free_wheels:
+		wheel.shaft.torque = 0.0
+		wheel.apply_torque(delta)
+	for wheel in wheels:
+		var brake := maxf(input_brake, input_hand_brake) if wheel.hand_brakable else input_brake
+		wheel.update_rotation(delta, free, brake)
