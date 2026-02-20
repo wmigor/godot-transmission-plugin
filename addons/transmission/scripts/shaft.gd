7402fd03e34e9a0f24d66c0aa526bfd143a44c2f
igor <wmigor@yandex.ru>
2026-02-20 13:12:57 +0300

    4x4

---
+++ addons/transmission/scripts/shaft.gd
@@ -0,0 +1,7 @@
+extends RefCounted
+class_name Shaft
+
+var angular_velocity: float
+var torque: float
+var brake_torque: float
+var inertia: float
--- demo/cars/skid_sound.gd
+++ demo/cars/skid_sound.gd
@@ -1,6 +1,6 @@
 extends AudioStreamPlayer3D
 
-@export_range(0.0, 1.0, 0.01) var min_skid_factor := 0.2
+@export_range(0.0, 1.0, 0.01) var min_skid_factor := 0.33
 
 @onready var _body := get_parent() as RigidBody3D
 
