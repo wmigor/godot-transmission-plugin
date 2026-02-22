a4cac3fccd7a29f70c1cf7bd0eb820abc6159190
igor <wmigor@yandex.ru>
2026-02-23 00:27:00 +0300

    solve differential equations by Gauss

---
+++ addons/largelinearalgebra/src/VectorN.h
@@ -0,0 +1,56 @@
+#ifndef VECTOR_N_H
+#define VECTOR_N_H
+
+#include "Declarations.h"
+#include <vector>
+
+namespace godot {
+
+class VectorN : public RefCounted {
+    GDCLASS(VectorN, RefCounted)
+
+private:
+    
+protected:
+    static void _bind_methods();
+
+	std::vector<double> values;
+    bool is_approximately_zero(double total_threshold) const;
+public:
+    friend class DenseMatrix;
+	friend class SparseMatrix;
+
+    VectorN() {};
+    ~VectorN() {};
+
+    static Ref<VectorN> filled(double value, int size);
+    static Ref<VectorN> from_packed_array(const PackedFloat64Array& from);
+
+    Ref<VectorN> clone() const;
+    void resize(int size);
+    int get_size() const { return values.size(); };
+
+    void set_element(int idx, double value);
+    double get_element(int idx) const;
+
+    double dot(Ref<VectorN> other) const;
+    void normalise();
+
+    Ref<VectorN> add(Ref<VectorN> other) const;
+    Ref<VectorN> subtract(Ref<VectorN> other) const;
+    void add_in_place(Ref<VectorN> other);
+    void subtract_in_place(Ref<VectorN> other);
+    void add_multiple_in_place(Ref<VectorN> other, double multiple);
+    void multiply_scalar_in_place(double scalar);
+
+    double length_squared() const;
+    double length() const;
+    Ref<DenseMatrix> column_vector() const;
+    Ref<DenseMatrix> row_vector() const;
+
+    PackedFloat64Array to_packed_array() const;
+};
+
+}
+
+#endif
--- demo/test/diff_test.gd
+++ demo/test/diff_test.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
 @onready var motor := $Motor as MotorView
+@onready var shaft1 := $Motor/InputShaft as ShaftView
+@onready var shaft2 := $Motor/InputShaft/CentralDifferential/LeftShaft as ShaftView
+@onready var shaft3 := $Motor/InputShaft/CentralDifferential/RightShaft as ShaftView
+
 var av: float
 var time: float
+
 
 func _physics_process(delta: float) -> void:
 	var old := motor.angular_velocity
 	motor.update_velocity(delta)
-	for i in 20:
-		motor.update(delta)
+	_solve2x2()
+	#for i in 20:
+		#motor.update(delta)
 	time += delta
 	if old < 1 and motor.angular_velocity >= 1:
 		print("shaft time: ", time)
@@ -19,3 +25,30 @@ func _physics_process(delta: float) -> void:
 	
 	#print(av, ' ', input_shaft.angular_velocity, ' ', 0.5 * ($InputShaft/CentralDifferential/LeftShaft.angular_velocity + $InputShaft/CentralDifferential/RightShaft.angular_velocity))
 	#print(int(rad_to_deg(input_shaft.angle)), ' ', int(rad_to_deg(0.5 * $InputShaft/CentralDifferential/RightShaft.angle)))
+
+
+func _solve2x2() -> void:
+	var inertia_matrix := DenseMatrix.zero(4)
+	inertia_matrix.set_element(0, 0, motor.inertia)
+	inertia_matrix.set_element(1, 1, shaft1.inertia)
+	inertia_matrix.set_element(2, 2, shaft2.inertia)
+	inertia_matrix.set_element(3, 3, shaft3.inertia)
+
+	var jacobian := DenseMatrix.from_packed_array(PackedFloat64Array([
+		1.0, -motor.gear, 0.0, 0.0,
+		0.0, -2.0, 1.0, 1.0
+	]), 2, 4)
+
+	var a := jacobian.multiply_dense(inertia_matrix.inverse()).multiply_dense(jacobian.transposed())
+	var b := VectorN.from_packed_array([
+		-(motor.angular_velocity - motor.gear * shaft1.angular_velocity),
+		-(-2.0 * shaft1.angular_velocity + shaft2.angular_velocity + shaft3.angular_velocity)
+	]).column_vector()
+	var x := a.solve(b)
+	if x != null:
+		var r := x.to_packed_array()
+		motor.angular_velocity += r[0] / motor.inertia
+		shaft1.angular_velocity += -motor.gear * r[0] / shaft1.inertia
+		shaft1.angular_velocity += -2.0 * r[1] / shaft1.inertia
+		shaft2.angular_velocity += 1.0 * r[1] / shaft2.inertia
+		shaft3.angular_velocity += 1.0 * r[1] / shaft3.inertia
