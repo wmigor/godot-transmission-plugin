fbe07a09de89ee81bb568b87854256d90b9fed72
igor <wmigor@yandex.ru>
2026-07-15 11:10:51 +0300

    differential

---
+++ demo/constraints/differential_constraint.gd
@@ -0,0 +1,40 @@
+extends Constraint
+class_name DifferencialConstraint
+
+var shaft_in: Shaft
+var shaft1: Shaft
+var shaft2: Shaft
+
+const j_in := 1.0
+const j_out = -0.5
+
+var effective_mass: float
+var accumulated_impulse: float
+
+var sliding: bool
+
+
+func _init(shaft_in_: Shaft, shaft1_: Shaft, shaft2_: Shaft):
+	shaft_in = shaft_in_
+	shaft1 = shaft1_
+	shaft2 = shaft2_
+
+
+func pre_step() -> void:
+	var k := j_in * j_in * shaft_in.inv_inertia + j_out * j_out * (shaft1.inv_inertia + shaft2.inv_inertia)
+	effective_mass = 1.0 / k
+
+	shaft_in.apply_torque_impulse(j_in * accumulated_impulse)
+	shaft1.apply_torque_impulse(j_out * accumulated_impulse)
+	shaft2.apply_torque_impulse(j_out * accumulated_impulse)
+
+
+func step(_delta: float) -> void:
+	var jv: float = j_in * shaft_in.angular_velocity + j_out * (shaft1.angular_velocity + shaft2.angular_velocity)
+	var lambda: float = -jv * effective_mass
+	
+	accumulated_impulse += lambda
+
+	shaft_in.apply_torque_impulse(j_in * lambda)
+	shaft1.apply_torque_impulse(j_out * lambda)
+	shaft2.apply_torque_impulse(j_out * lambda)
--- demo/constraints/view.gd
+++ demo/constraints/view.gd
@@ -2,20 +2,28 @@ extends Node2D
 
 var _solver := Solver.new()
 var _motor := Shaft.new(0.1)
-var _wheel := Shaft.new(0.3)
+var _wheel1 := Shaft.new(0.3)
+var _wheel2 := Shaft.new(0.3)
 var _clutch_shaft := Shaft.new(0.1)
+var _differencial_in := Shaft.new(0.3)
 var _clutch := ClutchConstraint.new(_motor, _clutch_shaft, 101.0)
-var _gearbox := GearConstraint.new(_clutch_shaft, _wheel, 2.0)
-var _brake := BrakeConstraint.new(_wheel, 0.1, 0.8, 3000.0)
+var _gearbox := GearConstraint.new(_clutch_shaft, _differencial_in, 2.0)
+var _differential := DifferencialConstraint.new(_differencial_in, _wheel1, _wheel2)
+var _brake1 := BrakeConstraint.new(_wheel1, 0.1, 0.8, 3000.0)
+var _brake2 := BrakeConstraint.new(_wheel2, 0.1, 0.8, 3000.0)
 
 
 func _ready() -> void:
-	_solver.shafts.append_array([_motor, _clutch_shaft, _wheel])
-	_solver.constraints.append_array([_clutch, _gearbox, _brake])
+	_solver.shafts.append_array([_motor, _clutch_shaft, _differencial_in, _wheel1, _wheel2])
+	_solver.constraints.append_array([_clutch, _gearbox, _differential, _brake1, _brake2])
 	_clutch.clutch_pedal = $Clutch.value
 	$Clutch.value_changed.connect(func(value: float): _clutch.clutch_pedal = value)
-	_brake.pedal = $Brake.value
-	$Brake.value_changed.connect(func(value: float): _brake.pedal = value)
+	_brake1.pedal = $Brake.value
+	$Brake.value_changed.connect(func(value: float): _brake1.pedal = value)
+	_brake2.pedal = $Brake2.value
+	$Brake2.value_changed.connect(func(value: float): _brake2.pedal = value)
+	_gearbox.shift_gear($Gear.value)
+	$Gear.value_changed.connect(func(value: float): _gearbox.shift_gear(value))
 
 
 func _physics_process(delta: float) -> void:
